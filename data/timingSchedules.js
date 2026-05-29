bd009e4c919f4e0cfcb8cfc261ff07718b1da4ce
elimorse0223-beep <eli.morse0223@gmail.com>
2026-05-29 14:08:44 -0400

    Add timing schedules for normal and ASM Friday

---
+++ data/timingSchedules.js
@@ -0,0 +1,155 @@
+export const timingSchedules = {
+
+  normal: {
+    name: "Normal Day",
+
+    events: [
+      {
+        id: "advisory",
+        name: "Advisory",
+        start: "08:00",
+        end: "08:05"
+      },
+
+      {
+        id: "block1",
+        name: "Block 1",
+        start: "08:10",
+        end: "09:10"
+      },
+
+      {
+        id: "block2",
+        name: "Block 2",
+        start: "09:15",
+        end: "10:15"
+      },
+
+      {
+        id: "block3",
+        name: "Block 3",
+        start: "10:20",
+        end: "11:20"
+      },
+
+      {
+        id: "earlyLunch",
+        name: "Early Lunch",
+        start: "11:20",
+        end: "11:45"
+      },
+
+      {
+        id: "lateLunch",
+        name: "Late Lunch",
+        start: "11:45",
+        end: "12:10"
+      },
+
+      {
+        id: "flex",
+        name: "Flex",
+        start: "12:10",
+        end: "12:35"
+      },
+
+      {
+        id: "block4",
+        name: "Block 4",
+        start: "12:35",
+        end: "13:35"
+      },
+
+      {
+        id: "block5",
+        name: "Block 5",
+        start: "13:40",
+        end: "14:40"
+      },
+
+      {
+        id: "collab",
+        name: "Collab",
+        start: "14:40",
+        end: "15:30"
+      }
+    ]
+  },
+
+  asmFriday: {
+    name: "ASM Friday",
+
+    events: [
+      {
+        id: "advisory",
+        name: "Advisory",
+        start: "08:00",
+        end: "08:05"
+      },
+
+      {
+        id: "block1",
+        name: "Block 1",
+        start: "08:10",
+        end: "09:10"
+      },
+
+      {
+        id: "block2",
+        name: "Block 2",
+        start: "09:15",
+        end: "10:15"
+      },
+
+      {
+        id: "block3",
+        name: "Block 3",
+        start: "10:20",
+        end: "11:20"
+      },
+
+      {
+        id: "earlyLunch",
+        name: "Early Lunch",
+        start: "11:20",
+        end: "11:45"
+      },
+
+      {
+        id: "lateLunch",
+        name: "Late Lunch",
+        start: "11:45",
+        end: "12:10"
+      },
+
+      {
+        id: "flex",
+        name: "Flex",
+        start: "12:10",
+        end: "12:35"
+      },
+
+      {
+        id: "allSchoolMeeting",
+        name: "All School Meeting",
+        start: "12:35",
+        end: "13:15"
+      },
+
+      {
+        id: "block4",
+        name: "Block 4",
+        start: "13:15",
+        end: "14:15"
+      },
+
+      {
+        id: "block5",
+        name: "Block 5",
+        start: "14:20",
+        end: "15:20"
+      }
+    ]
+  }
+
+};
